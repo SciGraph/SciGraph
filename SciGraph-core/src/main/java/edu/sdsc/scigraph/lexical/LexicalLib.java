da68b28890686ae3001162e2a2c36ea0a769dae3
Christopher Condit <condit@sdsc.edu>
2014-07-15 15:08:09 -0700

    PMD fixes

--- SciGraph-core/src/main/java/edu/sdsc/scigraph/lexical/LexicalLib.java
+++ SciGraph-core/src/main/java/edu/sdsc/scigraph/lexical/LexicalLib.java
@@ -26,25 +26,25 @@ public interface LexicalLib {
    * @param text A text fragment with multiple sentences
    * @return A list of the sentences found in the text
    */
-  public abstract List<String> extractSentences(String text);
+  List<String> extractSentences(String text);
 
   /***
    * @param sentence A single sentence.
    * @return A string with POS annotations
    */
-  public abstract List<PosToken> tagPOS(String sentence);
+  List<PosToken> tagPOS(String sentence);
 
   /***
    * @param text The text to chunk
    * @return a list of phrase chunks recognized in the sentence
    */
-  public abstract List<Token<String>> getChunks(String text);
+  List<Token<String>> getChunks(String text);
 
   /***
    * Attempt to recognize entities (not chunks) in text.
    * @param text A section of text
    * @return A list of recognized entities
    */
-  public abstract List<Token<String>> getEntities(String text);
+  List<Token<String>> getEntities(String text);
 
 }
