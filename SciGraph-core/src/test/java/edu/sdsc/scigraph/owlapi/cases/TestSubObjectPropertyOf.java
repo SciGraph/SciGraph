0162a19119d51252a15d9938f8ac60038655e191
Chris Mungall <cjm@berkeleybop.org>
2014-08-29 15:10:43 -0700

    Added failing test for sub-APs

---
+++ SciGraph-core/src/test/java/edu/sdsc/scigraph/owlapi/cases/TestSubObjectPropertyOf.java
@@ -0,0 +1,29 @@
+package edu.sdsc.scigraph.owlapi.cases;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.hamcrest.collection.IsIterableWithSize;
+import org.junit.Test;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+
+import edu.sdsc.scigraph.neo4j.GraphUtil;
+import edu.sdsc.scigraph.neo4j.OwlLabels;
+
+/***
+ * See https://github.com/SciCrunch/SciGraph/wiki/MappingToOWL#subclassof-axioms
+ *
+ */
+public class TestSubObjectPropertyOf extends OwlTestCase {
+
+  @Test
+  public void testSubclass() {
+    Node subp = getNode("http://example.org/subproperty");
+    Node superp = getNode("http://example.org/superproperty");
+    assertThat("subclass should be a directed relationship",
+        GraphUtil.getRelationships(subp, superp, OwlLabels.RDFS_SUB_PROPERTY_OF),
+        is(IsIterableWithSize.<Relationship> iterableWithSize(1)));
+  }
+
+}
