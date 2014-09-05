e0a636f2c61ac2024018731fc51678224cb4a917
Chris Mungall <cjm@berkeleybop.org>
2014-09-04 17:42:44 -0700

    renamed

--- SciGraph-core/src/test/java/edu/sdsc/scigraph/owlapi/cases/TestPun.java
+++ SciGraph-core/src/test/java/edu/sdsc/scigraph/owlapi/cases/TestPun.java
@@ -15,7 +15,7 @@ import edu.sdsc.scigraph.neo4j.GraphUtil;
 public class TestPun extends OwlTestCase {
 
   @Test
-  public void testObjectPropertyAssertion() {
+  public void testPun() {
     Node i = getNode("http://example.org/i");
     Node j = getNode("http://example.org/j");
     Node k = getNode("http://example.org/k");
@@ -25,6 +25,9 @@ public class TestPun extends OwlTestCase {
     assertThat("OPE edge should start with the subject.", relationship.getStartNode(), is(i));
     assertThat("OPE edge should start with the target.", relationship.getEndNode(), is(j));
     // TODO: also test that there is a SubClassOf edge between i and k
+    // i --[subClassOf]--> k
+    // the idea is that we may have punning in the ontology, but we want SciGraph to act
+    // in a similar manner to an RDF store and treat these as the same entity
   }
 
 }
