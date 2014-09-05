92f1ceed9993c924113eccf9fba1fa9be02b1da6
Chris Mungall <cjm@berkeleybop.org>
2014-09-04 17:37:14 -0700

    added (incomplete) punning test

---
+++ SciGraph-core/src/test/java/edu/sdsc/scigraph/owlapi/cases/TestPun.java
@@ -0,0 +1,30 @@
+package edu.sdsc.scigraph.owlapi.cases;
+
+import static com.google.common.collect.Iterables.getOnlyElement;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+import org.neo4j.graphdb.DynamicRelationshipType;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.RelationshipType;
+
+import edu.sdsc.scigraph.neo4j.GraphUtil;
+
+public class TestPun extends OwlTestCase {
+
+  @Test
+  public void testObjectPropertyAssertion() {
+    Node i = getNode("http://example.org/i");
+    Node j = getNode("http://example.org/j");
+    Node k = getNode("http://example.org/k");
+
+    RelationshipType p = DynamicRelationshipType.withName("p");
+    Relationship relationship = getOnlyElement(GraphUtil.getRelationships(i, j, p));
+    assertThat("OPE edge should start with the subject.", relationship.getStartNode(), is(i));
+    assertThat("OPE edge should start with the target.", relationship.getEndNode(), is(j));
+    // TODO: also test that there is a SubClassOf edge between i and k
+  }
+
+}
