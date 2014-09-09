3e9083ed0dcee11141b05d930195ae8fa2a4baec
Christopher Condit <condit@sdsc.edu>
2014-09-09 09:36:35 -0700

    Adds declaration for class and named instance

--- SciGraph-core/src/test/java/edu/sdsc/scigraph/owlapi/cases/TestClassAssertion.java
+++ SciGraph-core/src/test/java/edu/sdsc/scigraph/owlapi/cases/TestClassAssertion.java
@@ -9,7 +9,6 @@ import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
 
 import com.google.common.base.Optional;
-import com.google.common.collect.Iterables;
 
 import edu.sdsc.scigraph.frames.CommonProperties;
 import edu.sdsc.scigraph.neo4j.GraphUtil;
