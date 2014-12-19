60ced5e791e73130d5e5e04c9c6f3d39b6ead58c
Christopher Condit <condit@sdsc.edu>
2014-12-19 12:55:17 -0700

    Adds test for multiple refine queries

--- SciGraph-services/src/test/java/edu/sdsc/scigraph/services/refine/TestRefineQuery.java
+++ SciGraph-services/src/test/java/edu/sdsc/scigraph/services/refine/TestRefineQuery.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2014 The SciGraph authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package edu.sdsc.scigraph.services.refine;
 
 import static io.dropwizard.testing.FixtureHelpers.fixture;
@@ -10,6 +25,7 @@ import java.util.Map;
 
 import org.junit.Test;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class TestRefineQuery {
@@ -17,7 +33,7 @@ public class TestRefineQuery {
   private static final ObjectMapper MAPPER = Jackson.newObjectMapper();
 
   @Test
-  public void complexQuery() throws Exception {
+  public void deserializationIsCorrect_withAllFields() throws Exception {
     RefineQuery expected = new RefineQuery();
     expected.setQuery("foo");
     expected.setLimit(10);
@@ -28,6 +44,20 @@ public class TestRefineQuery {
     expected.setProperties(properties);
 
     RefineQuery actual = MAPPER.readValue(fixture("fixtures/refineQuery.json"), RefineQuery.class);
+    assertThat(actual, is(expected));
+  }
+  
+  @Test
+  public void deserializationIsCorrect_withMultipleQueries() throws Exception {
+    RefineQuery foo = new RefineQuery();
+    foo.setQuery("foo");
+    RefineQuery bar = new RefineQuery();
+    bar.setQuery("bar");
+    Map<String, RefineQuery> expected = new HashMap<>();
+    expected.put("q0", foo);
+    expected.put("q1", bar);
+    
+    Map<String, RefineQuery> actual = MAPPER.readValue(fixture("fixtures/refineQueries.json"), new TypeReference<HashMap<String,RefineQuery>>(){});
     assertThat(actual, is(expected));
   }
 
