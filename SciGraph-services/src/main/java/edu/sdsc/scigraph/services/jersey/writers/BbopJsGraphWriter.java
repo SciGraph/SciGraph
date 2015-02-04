fa1f3c4847991fefef28b18dcfb6cf553edbbf0d
Christopher Condit <condit@sdsc.edu>
2015-02-04 15:38:20 -0800

    Removes unused imports

--- SciGraph-services/src/main/java/edu/sdsc/scigraph/services/jersey/writers/BbopJsGraphWriter.java
+++ SciGraph-services/src/main/java/edu/sdsc/scigraph/services/jersey/writers/BbopJsGraphWriter.java
@@ -22,9 +22,7 @@ import java.io.OutputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Array;
 import java.lang.reflect.Type;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
 
 import javax.inject.Inject;
