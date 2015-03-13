add4334ee63f606bfca67477f1c2093446186f6d
Christopher Condit <condit@sdsc.edu>
2015-03-13 16:45:51 -0700

    Periodically commits transactions to improve speed.

--- SciGraph-core/src/main/java/edu/sdsc/scigraph/owlapi/OwlPostprocessor.java
+++ SciGraph-core/src/main/java/edu/sdsc/scigraph/owlapi/OwlPostprocessor.java
@@ -26,6 +26,7 @@ import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.DynamicLabel;
 import org.neo4j.graphdb.DynamicRelationshipType;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Path;
 import org.neo4j.graphdb.Relationship;
@@ -93,30 +94,43 @@ public class OwlPostprocessor {
   }
 
   void processCategory(Node root, RelationshipType type, Direction direction, String category) {
+    logger.info("Processing category: " + category);
+    int count = 0;
+    int batchSize = 10_000;
+    Label label = DynamicLabel.label(category);
+    Transaction tx = graphDb.beginTx();
     for (Path position : graphDb.traversalDescription().uniqueness(Uniqueness.NODE_GLOBAL)
         .depthFirst().relationships(type, direction)
         .relationships(OwlRelationships.RDF_TYPE, Direction.INCOMING)
         .relationships(OwlRelationships.OWL_EQUIVALENT_CLASS, Direction.BOTH).traverse(root)) {
       Node end = position.endNode();
-      if (!GraphUtil.getProperties(end, Concept.CATEGORY, String.class).contains(category)) {
-        GraphUtil.addProperty(end, Concept.CATEGORY, category);
+      GraphUtil.addProperty(end, Concept.CATEGORY, category);
+      end.addLabel(label);
+      if (0 == ++count % batchSize) {
+        logger.fine("Commiting " + count);
+        tx.success();
+        tx.close();
+        tx = graphDb.beginTx();
       }
-      end.addLabel(DynamicLabel.label(category));
     }
+    tx.success();
+    tx.close();
   }
 
   public void processCategories(Map<String, String> categories) {
-    try (Transaction tx = graphDb.beginTx()) {
-      for (Entry<String, String> category : categories.entrySet()) {
-        Node root = nodeIndex.get(CommonProperties.URI, category.getKey()).getSingle();
-        if (null == root) {
-          logger.warning("Failed to locate " + category.getKey() + " while processing categories");
-        } else {
-          processCategory(root, OwlRelationships.RDFS_SUBCLASS_OF, Direction.INCOMING,
-              category.getValue());
-        }
+    logger.info("Processing categories");
+    for (Entry<String, String> category : categories.entrySet()) {
+      Node root = null;
+      try (Transaction tx = graphDb.beginTx()) {
+        root = nodeIndex.get(CommonProperties.URI, category.getKey()).getSingle();
+        tx.success();
       }
-      tx.success();
+      if (null == root) {
+        logger.warning("Failed to locate " + category.getKey() + " while processing categories");
+      } else {
+        processCategory(root, OwlRelationships.RDFS_SUBCLASS_OF, Direction.INCOMING,
+            category.getValue());
+      }
     }
   }
 
