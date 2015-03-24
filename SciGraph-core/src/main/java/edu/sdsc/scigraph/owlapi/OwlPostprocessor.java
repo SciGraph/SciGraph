cc8a3cfb3a57997110f6c3b158318de9e3887178
Christopher Condit <condit@sdsc.edu>
2015-03-23 19:49:56 -0700

    Handles relationships with unknown fragments

--- SciGraph-core/src/main/java/edu/sdsc/scigraph/owlapi/OwlPostprocessor.java
+++ SciGraph-core/src/main/java/edu/sdsc/scigraph/owlapi/OwlPostprocessor.java
@@ -79,7 +79,8 @@ public class OwlPostprocessor {
         for (Relationship r : svf.getRelationships(OwlRelationships.FILLER)) {
           Node object = r.getEndNode();
           String relationshipName =
-              GraphUtil.getProperty(property, CommonProperties.FRAGMENT, String.class).get();
+              GraphUtil.getProperty(property, CommonProperties.FRAGMENT, String.class)
+              .or(GraphUtil.getProperty(property, CommonProperties.URI, String.class).get());
           RelationshipType type = DynamicRelationshipType.withName(relationshipName);
           String propertyUri =
               GraphUtil.getProperty(property, CommonProperties.URI, String.class).get();
