293ed9d76009e111b147b52d08367f82873976bb
Jeremy Nguyen Xuan <jnguyenxuan@lbl.gov>
2015-04-14 17:10:34 -0700

    [#96] adding POST request

--- SciGraph-services/src/main/java/edu/sdsc/scigraph/services/resources/AnalyzerService.java
+++ SciGraph-services/src/main/java/edu/sdsc/scigraph/services/resources/AnalyzerService.java
@@ -1,24 +1,24 @@
 /**
  * Copyright (C) 2014 The SciGraph authors
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package edu.sdsc.scigraph.services.resources;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -27,7 +27,11 @@ import javax.ws.rs.core.MediaType;
 import org.neo4j.graphdb.GraphDatabaseService;
 
 import com.codahale.metrics.annotation.Timed;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import edu.sdsc.scigraph.analyzer.AnalyzeRequest;
 import edu.sdsc.scigraph.analyzer.AnalyzerResult;
 import edu.sdsc.scigraph.analyzer.HyperGeometricAnalyzer;
 import edu.sdsc.scigraph.internal.GraphApi;
@@ -53,8 +57,23 @@ public class AnalyzerService extends BaseResource {
   @Timed
   public String analyze(@QueryParam("samples") List<String> samples,
       @QueryParam("ontologyClass") String ontologyClass, @QueryParam("path") String path) {
+    return runAnalysis(samples, ontologyClass, path);
+  }
+
+  @POST
+  @Timed
+  public String analyzePost(String body) throws JsonParseException, JsonMappingException,
+      IOException {
+    ObjectMapper mapper = new ObjectMapper();
+    AnalyzeRequest analyzeRequest = mapper.readValue(body, AnalyzeRequest.class);
+    // TODO enforce not null
+    return runAnalysis(analyzeRequest.getSamples(), analyzeRequest.getOntologyClass(),
+        analyzeRequest.getPath());
+  }
+
+  private String runAnalysis(List<String> sampleSet, String ontologyClass, String path) {
     HyperGeometricAnalyzer hyperGeometricAnalyzer = new HyperGeometricAnalyzer(graphDb);
-    List<AnalyzerResult> pValues = hyperGeometricAnalyzer.analyze(samples, ontologyClass, path);
+    List<AnalyzerResult> pValues = hyperGeometricAnalyzer.analyze(sampleSet, ontologyClass, path);
     String response = "";
     for (AnalyzerResult p : pValues) {
       response += p.getNodeId() + " " + p.getCount() + "\n";
