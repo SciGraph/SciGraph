9c4e4b09fd8e153007c2b83ac67720df7fbfbb21
Christopher Condit <condit@sdsc.edu>
2015-08-11 14:40:50 -0700

    Deals with interceptor warning #98

--- SciGraph-core/src/main/java/io/scigraph/owlapi/curies/CurieModule.java
+++ SciGraph-core/src/main/java/io/scigraph/owlapi/curies/CurieModule.java
@@ -15,16 +15,26 @@
  */
 package io.scigraph.owlapi.curies;
 
+import java.lang.reflect.Method;
+
 import com.google.inject.AbstractModule;
+import com.google.inject.matcher.AbstractMatcher;
 import com.google.inject.matcher.Matchers;
 
 public class CurieModule extends AbstractModule {
 
+  private static final class TransactionMethodMatcher extends AbstractMatcher<Method> {
+    @Override
+    public boolean matches(final Method method) {
+      return method.isAnnotationPresent(AddCuries.class) && !method.isSynthetic();
+    }
+  }
+
   @Override
   protected void configure() {
     CurieAdder adder = new CurieAdder();
+    bindInterceptor(Matchers.annotatedWith(AddCuries.class), new TransactionMethodMatcher(), adder);
     requestInjection(adder);
-    bindInterceptor(Matchers.any(), Matchers.annotatedWith(AddCuries.class), adder);
   }
 
 }
