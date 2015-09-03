dc637b7be8ce47cc4b6b1fcf720e3aeec799ad5c
Jeremy Nguyen Xuan <jnguyenxuan@lbl.gov>
2015-09-03 15:23:52 -0700

    [#118] use IRI instead of fragments

--- SciGraph-core/src/main/java/io/scigraph/internal/EvidenceAspect.java
+++ SciGraph-core/src/main/java/io/scigraph/internal/EvidenceAspect.java
@@ -40,10 +40,10 @@ import com.tinkerpop.blueprints.Vertex;
 public class EvidenceAspect implements GraphAspect {
 
   // TODO: Fix fragments for #118
-  static final RelationshipType HAS_SUBJECT = DynamicRelationshipType.withName("association_has_subject");
-  static final RelationshipType HAS_OBJECT = DynamicRelationshipType.withName("association_has_object");
-  static final RelationshipType EVIDENCE = DynamicRelationshipType.withName("RO_0002558");
-  static final RelationshipType SOURCE = DynamicRelationshipType.withName("source");
+  static final RelationshipType HAS_SUBJECT = DynamicRelationshipType.withName("http://purl.org/oban/association_has_subject");
+  static final RelationshipType HAS_OBJECT = DynamicRelationshipType.withName("http://purl.org/oban/association_has_object");
+  static final RelationshipType EVIDENCE = DynamicRelationshipType.withName("http://purl.obolibrary.org/obo/RO_0002558");
+  static final RelationshipType SOURCE = DynamicRelationshipType.withName("http://purl.org/dc/elements/1.1/source");
 
 
   private final GraphDatabaseService graphDb;
