88bb19fc31eabdfa83f95ab47343a3adfa28b5ea
Jeremy Nguyen Xuan <jnguyenxuan@lbl.gov>
2016-05-09 17:16:28 -0700

    work in progress, trying to fix lucene tests

--- SciGraph-core/src/test/java/io/scigraph/lucene/VocabularyAnalyzerTest.java
+++ SciGraph-core/src/test/java/io/scigraph/lucene/VocabularyAnalyzerTest.java
@@ -1,17 +1,15 @@
 /**
  * Copyright (C) 2014 The SciGraph authors
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package io.scigraph.lucene;
 
@@ -22,9 +20,8 @@ import io.scigraph.frames.NodeProperties;
 import java.io.IOException;
 
 import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.Field.Index;
 import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.document.StringField;
 import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
@@ -48,7 +45,7 @@ public class VocabularyAnalyzerTest {
 
   static void addDoc(IndexWriter writer, String term) throws CorruptIndexException, IOException {
     Document doc = new Document();
-    doc.add(new Field(NodeProperties.LABEL, term, Store.YES, Index.ANALYZED));
+    doc.add(new StringField(NodeProperties.LABEL, "hippocampus", Store.YES));
     writer.addDocument(doc);
   }
 
