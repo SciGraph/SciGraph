5719132f61a6ba7bdd22889e6ff3fc07b81d8b38
Gaurav Vaidya <gaurav@renci.org>
2020-06-16 19:30:30 -0400

    Added documentation.

--- SciGraph-entity/src/test/java/io/scigraph/annotation/EntityProcessorImplIncorrectIndexesTest.java
+++ SciGraph-entity/src/test/java/io/scigraph/annotation/EntityProcessorImplIncorrectIndexesTest.java
@@ -30,6 +30,11 @@ import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+/**
+ * I noticed that some of the indexes in my SciGraph output were incorrect. I created this test to diagnose why
+ * and when that was happening.
+ */
+
 public class EntityProcessorImplIncorrectIndexesTest {
   EntityProcessorImpl processor;
   EntityFormatConfiguration config;
@@ -37,8 +42,8 @@ public class EntityProcessorImplIncorrectIndexesTest {
   // Example text extracted from PMC7111660.
   final static String originalText = "A novel L-type lectin was required for the multiplication of WSSV in red swamp crayfish (Procambarus clakii). Fig.\\u00a02: Tissues distribution of PcL-lectin. (A) The expressions of PcL-lectin mRNA in the six tissues of crayfish. The extracted total mRNAs from hemocytes, heart, hepatopancreas, gills, stomach and intestine were subjected to qRT-PCR assay. The 18S rRNA was used as reference. (B) The expression of PcL-lectin protein in the six tissues of crayfish. The extracted total proteins <em> the six tissues were subjected to Western blot assay. \u03b2-actin was used as internal control. The experiments were repeated three times. Different letters represented significant changes between groups, p < 0.05. The tissue distributions and expression profiles of PcL-lectin were analyzed using qRT-PCR with the PcL-lectin specific primers RT-PcL-lecF and RT-PcL-lecR (Table 1). The 18S rRNA was used as the reference and was amplified with the primers 18S F and 18SR (Table 1). The qRT-PCR was programmed at 95 \u00b0C for 10 min, followed by 40 cycles at 95 \u00b0C for 10 s, 60 \u00b0C for 1 min. DNA melting analysis was performed to confirm the specificity of the amplified products. The obtained data were statistically analyzed and calculated using the threshold cycle method as previously described [16]. Lectins are group of molecules which are highly specific for the binding of carbohydrate. It was earliest discovered in leguminous plants, therefore it was named legume lectin (or L-type lectin). Thereafter, many other members of lectins have been identified in most kinds of organisms, such as fungi, animals and plants. L-type lectins (LTLs) possess a luminal carbohydrate recognition domain which can bind to saccharides. In vertebrates, four kinds of LTLs have been reported, namely endoplasmic reticulum Golgi intermediate compartment-53 (ERGIC-53), ERGIC-53 like protein (ERGL), 36 kDa vesicular integral membrane protein (VIP36), and VIP36 like protein (VIPL) [1]. However, only ERGIC-53 and VIP36 were identified in invertebrates [2].";
   final static String text = // originalText;
-          // Replacing the '<' and '>' characters prevent the HTML-like tags from being processed.
-          originalText.replace("<", "-").replace(">", "-");
+    // Replacing the '<' and '>' characters prevent the HTML-like tags from being processed.
+    originalText.replace("<", "-").replace(">", "-");
 
   // Example entities to test.
   Entity mockCrayfish = new Entity("crayfish", "http://purl.obolibrary.org/obo/NCBITaxon_6724");
@@ -50,26 +55,23 @@ public class EntityProcessorImplIncorrectIndexesTest {
   @Before
   public void setUp() throws Exception {
     // Set up an EntityFormatConfiguration.Builder that can be used for debugging.
-    config = new EntityFormatConfiguration.Builder(new StringReader(text))
-      .writeTo(new Writer() {
+    config = new EntityFormatConfiguration.Builder(new StringReader(text)).writeTo(new Writer() {
+      @Override
+      public void write(char[] cbuf, int off, int len) throws IOException {
+        // For debugging, we can print out each annotated segment here.
+        // System.err.println("Annotated segment: [" + String.valueOf(cbuf, off, len) + "]");
+      }
 
-        @Override
-        public void write(char[] cbuf, int off, int len) throws IOException {
-          // For debugging, we can print out each annotated segment here.
-          // System.err.println("Annotated segment: [" + String.valueOf(cbuf, off, len) + "]");
-        }
+      @Override
+      public void flush() throws IOException {
 
-        @Override
-        public void flush() throws IOException {
+      }
 
-        }
+      @Override
+      public void close() throws IOException {
 
-        @Override
-        public void close() throws IOException {
-
-        }
-      })
-      .get();
+      }
+    }).get();
 
     // Set up a basic EntityRecognizer for some mock objects.
     EntityRecognizer recognizer = mock(EntityRecognizer.class);
@@ -79,6 +81,7 @@ public class EntityProcessorImplIncorrectIndexesTest {
     when(recognizer.getCssClass()).thenReturn("mock");
     processor = new EntityProcessorImpl(recognizer);
 
+    // Set up expected annotations: these are the correct annotations we expect.
     expectedAnnotations.add(new EntityAnnotation(mockLectin, 15, 21));
     expectedAnnotations.add(new EntityAnnotation(mockCrayfish, 79, 87));
     expectedAnnotations.add(new EntityAnnotation(mockCrayfish, 220, 229));
@@ -87,8 +90,13 @@ public class EntityProcessorImplIncorrectIndexesTest {
     expectedAnnotations.add(new EntityAnnotation(mockLectin, 1484, 1492));
   }
 
+  /**
+   * In this test, we use both processor.getAnnotations and processor.annotateEntities to parse the same text.
+   * When the text contains an unclosed HTML tag, entities are still recognized correctly after that point, but
+   * the indices for their location in the text is incorrect.
+   */
   @Test
-  public void testGetAnnotations() throws IOException, InterruptedException {
+  public void testGetAnnotations() throws InterruptedException, IOException {
     List<EntityAnnotation> annotationsFromGetAnnotations = processor.getAnnotations(text, config);
     annotationsFromGetAnnotations.forEach(annotation -> {
       System.err.println(" - Found annotation using getAnnotations " + annotation + " in <" + text.substring(annotation.range.lowerEndpoint(), annotation.range.upperEndpoint()) + ">");
